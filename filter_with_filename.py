51c9d19e61863504bb14dd991b8badeb74bb83b2
frog016 <mixalyov.nikita@mail.ru>
2021-11-21 17:27:40 +0500

    Create filter_with_filename.py file

---
+++ filter_with_filename.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import numpy as np
+
+
+def calculate_gray_cell(row, column):
+    mosaic = pixels_matrix[row:min(row + cell_size, rows_count), column:min(column + cell_size, columns_count)]
+    return int((np.sum(mosaic) // 3) // (cell_size ** 2))
+
+
+def set_gray_in_cell(row, column):
+    mosaic = pixels_matrix[row:min(row + cell_size, rows_count), column:min(column + cell_size, columns_count)]
+    np.place(mosaic[:, ], mosaic >= 0, int(gray // gradation) * gradation)
+
+
+image = Image.open("test-img.jpg")
+cell_size = 10
+gradation = 255 // 50
+pixels_matrix = np.array(image)
+rows_count = len(pixels_matrix)
+columns_count = len(pixels_matrix[1])
+
+current_row = 0
+while current_row < rows_count:
+    current_column = 0
+    while current_column < columns_count:
+        gray = calculate_gray_cell(current_row, current_column)
+        set_gray_in_cell(current_row, current_column)
+        current_column = current_column + cell_size
+    current_row = current_row + cell_size
+
+Image.fromarray(pixels_matrix).save("res.jpg")
