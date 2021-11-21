af2852b2c31aaf95d6ebbd8ce556e4a2cf138bce
frog016 <mixalyov.nikita@mail.ru>
2021-11-21 17:56:05 +0500

    Add doctests and documentation.

--- filter_with_filename.py
+++ filter_with_filename.py
@@ -3,11 +3,28 @@ import numpy as np
 
 
 def calculate_gray_cell(row, column):
+    """
+    Takes a cell in a photo and calculates the average gray value over it. \n
+    :rtype: object
+    :param row: cell row number.
+    :param column: cell column number.
+    :return: int gray value.
+
+    >>> calculate_gray_cell(0, 0)
+    70
+    >>> calculate_gray_cell(5, 14)
+    72
+    """
     mosaic = pixels_matrix[row:min(row + cell_size, rows_count), column:min(column + cell_size, columns_count)]
     return int((np.sum(mosaic) // 3) // (cell_size ** 2))
 
 
 def set_gray_in_cell(row, column):
+    """
+    Sets the gray value to all pixels in a cell. \n
+    :param row: cell row number.
+    :param column: cell column number.
+    """
     mosaic = pixels_matrix[row:min(row + cell_size, rows_count), column:min(column + cell_size, columns_count)]
     np.place(mosaic[:, ], mosaic >= 0, int(gray // gradation) * gradation)
 
